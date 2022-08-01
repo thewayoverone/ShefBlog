ba7127f43df9a726900e3921b7d18e7c3e598106
kirill1881 <82600534+kirill1881@users.noreply.github.com>
2022-08-01 21:30:01 +0300

    123

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('comment/<int:pk>/',
+         views.CreateComment.as_view(), name="create_comment"),
+    path('<slug:slug>/<slug:post_slug>/',
+         views.PostDetailView.as_view(), name="post_single"),
+    path('<slug:slug>/', views.PostListView.as_view(),
+         name="post_list"),
+    path('', views.HomeView.as_view(), name="home"),
+]
